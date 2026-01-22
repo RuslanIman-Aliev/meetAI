ebf28890d7f998834cfc1049e05d204de84b0016
RuslanIman-Aliev <s0970802047@gmail.com>
2026-01-22 13:13:21 +0100

    09: agents setup

---
+++ src/modules/agents/ui/views/agent-view.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export const AgentsView = () => {
+  const trpc = useTRPC();
+  const { data} = useSuspenseQuery(
+    trpc.agents.getMany.queryOptions(),
+  );
+  
+  return <></>;
+};
+
+export const AgentsViewLoading = ()=>{
+  return (
+    <LoadingState
+      title="Loading Agents"
+      description="Please wait while we load the agents."
+    />
+  )
+}
+
+export const AgentsViewError = ()=>{
+  return (
+    <ErrorState 
+      title="Failed to Load Agents"
+      description="An error occurred while loading the agents. Please try again."
+    />
+  )
+}
+    
