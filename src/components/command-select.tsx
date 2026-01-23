e2b001e65003955666513bf588c8c0b2e46b8e6c
RuslanIman-Aliev <s0970802047@gmail.com>
2026-01-23 20:07:42 +0100

    17: meetings form

---
+++ src/components/command-select.tsx
@@ -0,0 +1,75 @@
+import { ReactNode, useState } from "react";
+import { Button } from "./ui/button";
+import { ChevronsUpDownIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+  CommandEmpty,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandResponsiveDialog,
+} from "./ui/command";
+
+interface Props {
+  options: Array<{
+    id: string;
+    value: string;
+    children: ReactNode;
+  }>;
+  onSelect: (value: string) => void;
+  onSearch?: (value: string) => void;
+  value: string;
+  placeholder?: string;
+  isSearchable?: boolean;
+  className?: string;
+}
+
+export const CommandSelect = ({
+  options,
+  onSelect,
+  onSearch,
+  value,
+  placeholder = "Select an option",
+  className,
+}: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const selectedOption = options.find((option) => option.value === value);
+
+  return (
+    <>
+      <Button
+        onClick={() => setIsOpen(true)}
+        className={cn(
+          "h-9 justify-between font-normal px-2",
+          !selectedOption && "text-muted-foreground",
+          className,
+        )}
+      >
+        <div>{selectedOption ? selectedOption.children : placeholder}</div>
+        <ChevronsUpDownIcon />
+      </Button>
+
+      <CommandResponsiveDialog open={isOpen} onOpenChange={setIsOpen} shouldFilter={!onSearch}>
+        <CommandInput placeholder="Search...." onValueChange={onSearch} />
+        <CommandList>
+          <CommandEmpty>
+            <span className="text-muted-foreground text-sm">
+              No options found.
+            </span>
+          </CommandEmpty>
+          {options.map((option) => (
+            <CommandItem
+              key={option.id}
+              onSelect={() => {
+                onSelect(option.value);
+                setIsOpen(false);
+              }}
+            >
+              {option.children}
+            </CommandItem>
+          ))}
+        </CommandList>
+      </CommandResponsiveDialog>
+    </>
+  );
+};
